0b6eff76d6bc46d22558c8652cc5bdd77a2a49a9
crynobone <crynobone@gmail.com>
2011-09-01 23:30:31 +0800

    Code indentation

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,18 +1,18 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
+    '_root_'  => 'welcome/index',  // The default route
+    '_404_'   => 'welcome/404',    // The main 404 route
 
     'page/:number'          => 'site/index',
 
     'p/:short_uri'          => 'site/post',
     'post/:id/:long_uri'    => 'site/post',
     'post/:id'              => 'site/post',
-	
-	/**
-	 * This is an example of a BASIC named route (used in reverse routing).
-	 * The translated route MUST come first, and the 'name' element must come
-	 * after it.
-	 */
-	// 'foo/bar' => array('welcome/foo', 'name' => 'foo'),
+    
+    /**
+     * This is an example of a BASIC named route (used in reverse routing).
+     * The translated route MUST come first, and the 'name' element must come
+     * after it.
+     */
+    // 'foo/bar' => array('welcome/foo', 'name' => 'foo'),
 );
